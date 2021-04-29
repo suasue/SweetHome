b699bb2eadb34a8b5ca6bd08db46d81fa423e562
suasue <devsu7477@gmail.com>
2021-04-29 20:34:11 +0900

    Modify: signup validation 수정

--- user/views.py
+++ user/views.py
@@ -1,49 +1,58 @@
 import json
 import bcrypt
 import jwt
+import re
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
-from my_settings  import SECRET_KEY, ALGORITHM
-from .utils       import login_decorator
-from .models      import User
+from my_settings import SECRET_KEY, ALGORITHM
+from .utils      import login_decorator
+from .models     import User
 
 MINIMUM_PASSWORD_LENGTH = 8
-MINIMUM_NAME_LENGTH = 2
-MAXIMUN_NAME_LENGTH = 15
+MINIMUM_NAME_LENGTH     = 2
+MAXIMUN_NAME_LENGTH     = 15
 
 class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            email    = data['email']
-            password = data['password']                                
-            name     = data['name']
+
+            email    = data.get('email')
+            password = data.get('password')                               
+            name     = data.get('name')
+
+            email_pattern = re.compile('[^@]+@[^@]+\.[^@]+')
+
+            if not (email and password and name):
+                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+            if not re.match(email_pattern, email):
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
 
             if len(password) < MINIMUM_PASSWORD_LENGTH:
-                return JsonResponse({'message' : 'INVALID_PASSWORD'}, status = 400)
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
 
-            if not email:
-                return JsonResponse({'message' : 'INVALID_EMAIL'}, status = 400)
+            if len(name) < MINIMUM_NAME_LENGTH or len(name) > MAXIMUN_NAME_LENGTH:
+                return JsonResponse({'message':'INVALID_NAME'}, status=400)
 
-            if len(name) < MINIMUM_NAME_LENGTH and len(name) > MAXIMUN_NAME_LENGTH:
-                return JsonResponse({'message' : 'INVALID_NAME'}, status = 400)
-
-            if User.objects.filter(name = name).exists():
-                return JsonResponse({'message' : 'USER_ALREADY_EXISTS'}, status = 400)
+            if User.objects.filter(Q(name=name) | Q(email=email)).exists():
+                return JsonResponse({'message':'USER_ALREADY_EXISTS'}, status=409)
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             User.objects.create(
                 email    = email,
-                password = hashed_password,
+                password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                 name     = name,
             )
-            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+            return JsonResponse({'message':'SUCCESS'}, status=201)
                 
-        except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+        except ValueError:
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
 
 
 class SigninView(View):
